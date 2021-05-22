018b39460edd9918aedc148f8c2aa148374f7c54
lawliet022 <deepak26022@gmail.com>
2021-05-22 20:12:45 +0530

    Ep02

--- main.go
+++ main.go
@@ -1,42 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"example.com/hello/tutorial2/handlers"
 )
 
 func main() {
 
-	// "/" -  default for all patterns if not matched to any
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
-		data, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags) // a new Logger (maybe to a database)
 
+	hh := handlers.NewHello(l)   // hello handler
+	gh := handlers.NewGoodbye(l) // goodbye handler
+
+	sm := http.NewServeMux() // serve mux
+	sm.Handle("/", hh)       // Register any path to hello handler
+	sm.Handle("/bye", gh)    // Register bye path to goodbye handler
+
+	s := &http.Server{ // custom server
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Ooops", http.StatusBadRequest)
-			// Or
-			// w.WriteHeader(http.StatusBadRequest)
-			// w.Write([]byte("Ooops"))
-			return
+			l.Fatal(err) // Fatal exists with a status code of err
 		}
+	}()
 
-		log.Printf("Data - %s\n", data)
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-		// Because responseWriter implements ioWriter
-		fmt.Fprintf(w, "Hello %s\n", data)
+	sig := <-sigChan
+	l.Println("Received terminate. Gracefully shutting down - ", sig)
 
-		// Or using write method
-		// w.Write([]byte(fmt.Sprintf("Hello %s", data)))
-	})
+	// wait for 30 sec to gracefully finish all work, after that force close.
+	// Server do not accepts new requests in this 30 sec of time
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second) // timeout context
+	defer cancel()                                                          // releases resources if ops completes before timeout elapses
 
-	// HandleFunc converts a func to a handler and registers it to a ServeMux
-	// (a server multiplexer -> basically mapping pattern/path to a func)
-	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
-		log.Println("GoodBye World")
-	})
-
-	http.ListenAndServe(":9090", nil)
+	s.Shutdown(tc)
 
 }
